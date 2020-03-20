3809a3937341307ac194a289ef0d3ad1b6f07bb5
Howard <howardmkeane@hotmail.co.uk>
2020-03-20 00:55:12 +0000

    initial commit of c# Darmark

---
+++ Darmark/Darmark/Program.cs
@@ -0,0 +1,21 @@
+﻿using Darmark.Properties;
+using EireScript;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Darmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot configs = new ConfigurationBuilder().AddCommandLine(args).Build();
+            string[] InputFiles = configs[Resources.InputCommandLineParameter]?.Split(Resources.InputCommandLineParameterDelimiter)
+                                                                               .Select(fn => fn.Trim())
+                                                                               .ToArray();
+            Compiler compiler = new Compiler(InputFiles);
+            compiler.Compile();
+        }
+    }
+}
---
+++ Darmark/EireScript/Compiler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace EireScript
+{
+    public class Compiler
+    {
+        private string[] filenames;
+        public Compiler(string[] filenames)
+        {
+            this.filenames = filenames;
+        }
+
+        public void Compile()
+        {
+            foreach(string filename in this.filenames)
+            {
+                Console.WriteLine($"Processing {filename}");
+            }
+        }
+    }
+}
